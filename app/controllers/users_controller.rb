0882bb2a37328d8e19c9fb2c52888eab03e41771
kei53 <kaiji303@gmail.com>
2020-04-03 22:13:29 +0900

    show-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,21 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,8 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'ce4dcfab048e8c5ee873da462f642d7d040b3400c7138cc12be77a8d9d5e5f3bb557d9cea33db5df184a9007d2e03c7bea7643acc372e51d80d863a878184442'
+
+
